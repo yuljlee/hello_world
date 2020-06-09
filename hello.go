e41bf233361532a1911519c6b696cc94180acd54
Yul Jae Lee <yuljlee9@gmail.com>
2020-06-08 23:23:20 -0700

    chore: Test if else

--- hello.go
+++ hello.go
@@ -1,12 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
-	var i int
-	var f float64
-	var b bool
-	var s string
-	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
 }

81a61929b7f6342f1d073f7ad8518f1eb7f73d3b
Yul Jae Lee <yuljlee9@gmail.com>
2020-06-09 18:16:30 -0700

    chore: Add os check code

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"runtime"
 )
 
 func pow(x, n, lim float64) float64 {
@@ -20,4 +21,16 @@ func main() {
 		pow(3, 2, 10),
 		pow(3, 3, 20),
 	)
+
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.\n", os)
+	}
 }
